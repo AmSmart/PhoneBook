02793c29b1769ca431bd2d4d89e47df164c34ed0
Adebiyi Emmanuel <smartemma03@gmail.com>
2019-10-07 09:38:51 +0100

    Add project files.

---
+++ PhoneBook/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace PhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._radioName = new System.Windows.Forms.RadioButton();
+            this._radioEmail = new System.Windows.Forms.RadioButton();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnCancell = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textName
+            // 
+            this.textName.Enabled = false;
+            this.textName.Location = new System.Drawing.Point(157, 31);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(232, 20);
+            this.textName.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 245);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(736, 197);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // textNo
+            // 
+            this.textNo.Enabled = false;
+            this.textNo.Location = new System.Drawing.Point(157, 104);
+            this.textNo.Name = "textNo";
+            this.textNo.Size = new System.Drawing.Size(178, 20);
+            this.textNo.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mobile No.";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Enabled = false;
+            this.textEmail.Location = new System.Drawing.Point(157, 66);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(194, 20);
+            this.textEmail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Location = new System.Drawing.Point(456, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(16, 232);
+            this.panel1.TabIndex = 7;
+            // 
+            // _radioName
+            // 
+            this._radioName.AutoSize = true;
+            this._radioName.Location = new System.Drawing.Point(580, 50);
+            this._radioName.Name = "_radioName";
+            this._radioName.Size = new System.Drawing.Size(68, 17);
+            this._radioName.TabIndex = 12;
+            this._radioName.TabStop = true;
+            this._radioName.Text = "By Name";
+            this._radioName.UseVisualStyleBackColor = true;
+            // 
+            // _radioEmail
+            // 
+            this._radioEmail.AutoSize = true;
+            this._radioEmail.Location = new System.Drawing.Point(580, 84);
+            this._radioEmail.Name = "_radioEmail";
+            this._radioEmail.Size = new System.Drawing.Size(65, 17);
+            this._radioEmail.TabIndex = 13;
+            this._radioEmail.TabStop = true;
+            this._radioEmail.Text = "By Email";
+            this._radioEmail.UseVisualStyleBackColor = true;
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(507, 12);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(232, 20);
+            this.textSearch.TabIndex = 11;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(532, 136);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Orange;
+            this.panel2.Location = new System.Drawing.Point(456, 217);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(372, 18);
+            this.panel2.TabIndex = 11;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(63, 159);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(60, 23);
+            this.btnNew.TabIndex = 8;
+            this.btnNew.Text = "Add New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(220, 159);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(51, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(139, 159);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(57, 23);
+            this.btnEdit.TabIndex = 9;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(368, 159);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(46, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(627, 136);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 16;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnCancell
+            // 
+            this.btnCancell.Location = new System.Drawing.Point(293, 159);
+            this.btnCancell.Name = "btnCancell";
+            this.btnCancell.Size = new System.Drawing.Size(58, 23);
+            this.btnCancell.TabIndex = 17;
+            this.btnCancell.Text = "Cancel";
+            this.btnCancell.UseVisualStyleBackColor = true;
+            this.btnCancell.Click += new System.EventHandler(this.btnCancell_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(827, 454);
+            this.Controls.Add(this.btnCancell);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this._radioEmail);
+            this.Controls.Add(this._radioName);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton _radioName;
+        private System.Windows.Forms.RadioButton _radioEmail;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnCancell;
+    }
+}
+
---
+++ PhoneBook/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneBook
+{
+    public partial class Form1 : Form
+    {   
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        DataTable table;
+        int _id;
+        bool addNew;
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Engineer Smart\source\repos\PhoneBook\PhoneBook\Contacts1.mdf;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textName.BackColor = textEmail.BackColor = textNo.BackColor = Color.DarkGray;
+            UpdateData();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            textEmail.Enabled = textName.Enabled = textNo.Enabled = btnSave.Enabled = true;
+            textName.BackColor = textEmail.BackColor = textNo.BackColor = Color.White;
+            textName.Focus();
+            _radioEmail.Enabled = _radioName.Enabled = textSearch.Enabled
+                = btnEdit.Enabled = btnSearch.Enabled = btnCancel.Enabled = false;
+            dataGridView1.Enabled =  false;
+            dataGridView1.ForeColor = Color.DarkGray;
+            addNew = true;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(textSearch.Text == "")
+            {
+                MessageBox.Show(this,"No valid keyword");
+                return;
+            }
+            if(_radioEmail.Checked == false &&_radioName.Checked == false)
+            {
+                MessageBox.Show(this,"Search criteria not selected");
+                return;
+            }
+            string value;
+            if (_radioName.Checked)
+            {
+                value = "Name";
+            }
+            else
+            {
+                value = "Email";
+            }
+
+            
+            adapter = new SqlDataAdapter("SELECT * FROM ContactTable WHERE " +
+                $"{value} LIKE '{textSearch.Text}%'", connection);
+            table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(textName.Text == "" || textNo.Text == "")
+            {
+                MessageBox.Show(this, "An important field is empty");
+                return;
+            }
+
+
+            _radioEmail.Enabled = _radioName.Enabled = textSearch.Enabled
+                = btnEdit.Enabled = btnSearch.Enabled = btnCancel.Enabled = true;
+            textEmail.Enabled = textName.Enabled = textNo.Enabled = btnSave.Enabled = false;
+            textName.BackColor = textEmail.BackColor = textNo.BackColor = Color.DarkGray;
+
+            connection.Open();
+
+            if (addNew)
+            {
+                command = new SqlCommand("SELECT Count(*) FROM ContactTable", connection);
+                int _nCount = Convert.ToInt32(command.ExecuteScalar()) + 1;
+                command = new SqlCommand("INSERT INTO ContactTable(SNo,Name,Email,MobileNo)VALUES" +
+                    "('" + _nCount + "','" + textName.Text + "','" + textEmail.Text + "','" + textNo.Text + "')"
+                    , connection);
+            }
+            else
+            {
+                command = new SqlCommand($"UPDATE ContactTable SET Name = '{textName.Text}', " +
+                    $"Email =  '{textEmail.Text}', MobileNo = '{textNo.Text}' " +
+                    $"WHERE SNo = {_id}", connection);
+            }
+
+            
+            
+            command.ExecuteNonQuery();
+            connection.Close();
+            UpdateData();
+            textEmail.Text = textName.Text = textNo.Text = "";
+            dataGridView1.Enabled = true;
+            dataGridView1.ForeColor = Color.Black;
+
+        }
+
+        private void UpdateData()
+        {
+            adapter = new SqlDataAdapter("SELECT * FROM ContactTable ORDER BY SNo", connection);
+            table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                    column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        
+            }
+            dataGridView1.BackgroundColor = Color.White;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Enabled = false;
+            dataGridView1.ForeColor = Color.DarkGray;
+            addNew = false;
+            textEmail.Enabled = textName.Enabled = textNo.Enabled = btnSave.Enabled = true;
+            textName.BackColor = textEmail.BackColor = textNo.BackColor = Color.White;
+            textName.Focus();
+            _radioEmail.Enabled = _radioName.Enabled = textSearch.Enabled
+                = btnEdit.Enabled = btnSearch.Enabled = btnCancel.Enabled = false;
+
+            int _selected = dataGridView1.CurrentCell.RowIndex + 1;
+            adapter = new SqlDataAdapter("SELECT * FROM ContactTable WHERE SNo = '" + _selected + "'", connection);
+            table = new DataTable();
+            adapter.Fill(table);
+            textName.Text = table.Rows[0][1].ToString();
+            textEmail.Text = table.Rows[0][2].ToString();
+            textNo.Text = table.Rows[0][3].ToString();
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult _decision = MessageBox.Show(this,"Are you sure you want to delete selected contact?",
+                "Confirm delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(_decision == DialogResult.Yes)
+            {
+                connection.Open();
+                command = new SqlCommand($"DELETE FROM ContactTable WHERE SNo = {_id}; UPDATE ContactTable SET " +
+                    $"SNo=SNo-1 WHERE SNo > {_id};", connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                UpdateData();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnEdit.Enabled = true;
+            
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnEdit_Click(sender, e);
+            dataGridView1.Enabled = false;
+            dataGridView1.ForeColor = Color.DarkGray;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            UpdateData();
+            textSearch.Text = "";
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnCancell_Click(object sender, EventArgs e)
+        {
+            textSearch.Text = textNo.Text = textEmail.Text = textName.Text = "";
+            dataGridView1.Enabled = true;
+            dataGridView1.ForeColor = Color.Black;
+            UpdateData();
+
+            btnSave.Enabled = false;
+            _radioEmail.Enabled = _radioName.Enabled = textSearch.Enabled 
+                = btnEdit.Enabled = btnSearch.Enabled = btnCancel.Enabled = true;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            _id = dataGridView1.CurrentCell.RowIndex + 1;
+        }
+    }
+}
